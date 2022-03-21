d1141fdbbcdba4b069f004b0af99c78b743f590f
lansinganthony85 <lansinganthony85@gmail.com>
2022-03-20 23:58:44 -0400

    Added scoring mechanic and begun game over screen

--- runner.py
+++ runner.py
@@ -3,6 +3,14 @@ from tkinter import CENTER
 import pygame
 from sys import exit
 
+
+def display_score():
+    current_time = (pygame.time.get_ticks() - start_time) // 1000
+    score_surf = test_font.render(f"Score: {current_time}", False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center=(400, 50))
+    screen.blit(score_surf, score_rect)
+
+
 pygame.init()
 WIDTH = 800
 HEIGHT = 400
@@ -10,25 +18,27 @@ FPS = 60
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
-test_font = pygame.font.Font("Python Runner/font/Pixeltype.ttf", 50)
-game_active = True
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+game_active = False
+start_time = 0
 
-sky_surface = pygame.image.load("Python Runner/graphics/Sky.png").convert()
-ground_surface = pygame.image.load("Python Runner/graphics/ground.png").convert()
+sky_surface = pygame.image.load("graphics/Sky.png").convert()
+ground_surface = pygame.image.load("graphics/ground.png").convert()
 
-score_surface = test_font.render("My game", False, (64, 64, 64))
-score_rect = score_surface.get_rect(center=(WIDTH / 2, 50))
+# score_surface = test_font.render("My game", False, (64, 64, 64))
+# score_rect = score_surface.get_rect(center=(WIDTH / 2, 50))
 
-snail_surface = pygame.image.load(
-    "Python Runner/graphics/snail/snail1.png"
-).convert_alpha()
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surface.get_rect(bottomleft=(800, 300))
 
-player_surface = pygame.image.load(
-    "Python Runner/graphics/player/player_walk_1.png"
-).convert_alpha()
+player_surface = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
 player_rect = player_surface.get_rect(midbottom=(80, 300))
 player_gravity = 0
+
+# Intro screen
+player_stand = pygame.image.load("graphics/player/player_stand.png").convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center=(400, 200))
 
 while True:
     for event in pygame.event.get():
@@ -46,12 +56,14 @@ while True:
             if event.type == pygame.KEYDOWN and pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = pygame.time.get_ticks()
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, "#c0e8ec", score_rect)
-        screen.blit(score_surface, score_rect)
+        # pygame.draw.rect(screen, "#c0e8ec", score_rect)
+        # screen.blit(score_surface, score_rect)
+        display_score()
 
         snail_rect.x -= 4
         if snail_rect.right < 0:
@@ -68,7 +80,8 @@ while True:
         if snail_rect.colliderect(player_rect):
             game_active = False
     else:
-        screen.fill("Yellow")
+        screen.fill((94, 129, 162))
+        screen.blit(player_stand, player_stand_rect)
 
     pygame.display.update()
     clock.tick(FPS)
